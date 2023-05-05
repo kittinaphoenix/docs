c3e890d03ccc6b94e67fc5667dafa418f43cc183
Hector <hector.oviedo@adgs.com>
2023-05-05 22:59:12 +0300

    im not a punchbag

---
+++ src/sections.template.js
@@ -0,0 +1,18 @@
+const sections = [
+{
+"type":"direct",
+"label":"Section1",
+"url":"sections/section1.html"
+},
+{
+"type":"tree",
+"label":"Tree Sections",
+"tree":[
+{
+"type":"direct",
+"label":"Section2",
+"url":"sections/docs/section2.html",
+}
+]
+}
+]
